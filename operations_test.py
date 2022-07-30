0baecd6f2bde9be57c4281f432b87cf25b97c8f9
DrBenvey <drbenvey1996@mail.ru>
2022-07-30 17:09:29 +0300

    parallel graf

--- operations_test.py
+++ operations_test.py
@@ -31,6 +31,66 @@ class SpeedTest:
         plt.ylabel('Time, s', fontsize=12, color='blue')
         plt.show()
 
+    def Testλμ_convolution_product_sequential_vs_parallel(self):
+        λ=3;μ=5;n=2;ν=9
+        print('λ=', λ,'μ=',μ,'n=',n,'ν=',ν, sep=' ')
+        l_indexes=np.arange(start=9, stop=17, step=1)
+        y_axis_parallel = [0] * len(l_indexes)
+        y_axis_sequential = [0] * len(l_indexes)
+        dim_b=λ+μ+ν
+        B=help.GetMM(dim_b,n)
+        for i in range(len(l_indexes)):
+            k=l_indexes[i]
+            dim_a=λ+μ+k
+            A=help.GetMM(dim_a,n)
+            start_time = time.time()
+            C1=operations.λμ_convolution_product_parallel(λ,μ,A,B)
+            end_time = time.time()
+            y_axis_parallel[i]=end_time - start_time
+            print('k=',k,'dim_a=',dim_a,'time parallel (s): ',y_axis_parallel[i],sep=' ')
+            start_time = time.time()
+            C2=operations.λμ_convolution_product_sequential(λ,μ,A,B)
+            end_time = time.time()
+            y_axis_sequential[i]=end_time - start_time
+            print('k=',k,'dim_a=',dim_a,'time sequential (s): ',y_axis_sequential[i],sep=' ')
+        data = {'NumPy_parallel':y_axis_parallel,'NumPy_sequential':y_axis_sequential}
+        plt.plot(l_indexes,y_axis_parallel,color='r')
+        plt.scatter(l_indexes,y_axis_parallel,color='r')
+        plt.plot(l_indexes,y_axis_sequential,color='b',linestyle='--')
+        plt.scatter(l_indexes,y_axis_sequential,color='b')
+        plt.legend(data, loc=2)
+        plt.grid(True)
+        plt.xlabel('N', fontsize=12, color='blue')
+        plt.ylabel('Time, s', fontsize=12, color='blue')
+        plt.show()
+
+    
+    def Testλμ_convolution_product_parallel(self):
+        λ=3;μ=5;n=2;ν=9
+        print('λ=', λ,'μ=',μ,'n=',n,'ν=',ν, sep=' ')
+        l_indexes=np.arange(start=9, stop=17, step=1)
+        y_axis_numpy = [0] * len(l_indexes)
+        dim_b=λ+μ+ν
+        B=help.GetMM(dim_b,n)
+        for i in range(len(l_indexes)):
+            k=l_indexes[i]
+            dim_a=λ+μ+k
+            A=help.GetMM(dim_a,n)
+            start_time = time.time()
+            C=operations.λμ_convolution_product_parallel(λ,μ,A,B)
+            end_time = time.time()
+            y_axis_numpy[i]=end_time - start_time
+            print('k=',k,'dim_a=',dim_a,'time(s): ',y_axis_numpy[i],sep=' ')
+        data = {'NumPy':y_axis_numpy}
+        plt.plot(l_indexes,y_axis_numpy,color='b',linestyle='--')
+        plt.scatter(l_indexes,y_axis_numpy,color='b')
+        plt.legend(data, loc=2)
+        plt.grid(True)
+        plt.xlabel('N', fontsize=12, color='blue')
+        plt.ylabel('Time, s', fontsize=12, color='blue')
+        plt.show()
+
+
 class Test:
     def __init__(self):
         self.n = 2 # Порядок многомерной матрицы
---
+++ usage_test.py
@@ -0,0 +1,15 @@
+import operations_test
+import help_test
+
+#_help_test = help_test.Test(2, 4)
+#_help_test.TestMM()
+#_help_test.SectionsMM()
+
+#_operations_test=operations_test.Test()
+#_operations_test.Testλμ_convolution_product_sequential()
+#_operations_test.Testλμ_convolution_product_parallel()
+_operations_speed_test=operations_test.SpeedTest()
+#_operations_speed_test.Testλμ_convolution_product_sequential()
+_operations_speed_test.Testλμ_convolution_product_parallel()
+_operations_speed_test.Testλμ_convolution_product_sequential_vs_parallel()
+
